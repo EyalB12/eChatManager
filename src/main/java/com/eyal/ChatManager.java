cc8099a43b03f3beae7845075cc74761a04d3727
BuildTools <unconfigured@null.spigotmc.org>
2017-01-16 20:23:22 +0200

    Initial release.

---
+++ src/main/java/com/eyal/ChatManager.java
@@ -0,0 +1,19 @@
+package com.eyal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This file was created by Eyal B. alreaedy known as Keido on Intelli-J IDEA.
+ * Any violation, attempt or try to leak, use or give this file without Eyal's
+ * permission may result in a penalty of up to $1,000 USD.
+ * <p>
+ * File was created on 16/01/2017.
+ * Copyright (c) 2017. Eyal B.
+ */
+public class ChatManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
