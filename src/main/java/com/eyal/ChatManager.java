8995c87f5c718c574a8ce167e9c693f3b3eaedc6
BuildTools <unconfigured@null.spigotmc.org>
2017-01-16 20:39:16 +0200

    Added many shit

--- src/main/java/com/eyal/ChatManager.java
+++ src/main/java/com/eyal/ChatManager.java
@@ -1,5 +1,9 @@
 package com.eyal;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -15,5 +19,46 @@ public class ChatManager extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
+
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("echatcmanager")) {
+            if(args.length == 0 || args[0].equalsIgnoreCase("help")) {
+                sender.sendMessage("§e§lECHATMANAGER COMMANDS:");
+                sender.sendMessage("§6/ecm clearchat <player>");
+                sender.sendMessage("§6/ecm watchchat [player]");
+                sender.sendMessage("§6/ecm help");
+                sender.sendMessage("§6More to come soon!");
+                return true;
+            }
+
+            if(args.length == 1) {
+                if(args[0].equalsIgnoreCase("clearchat")) {
+                    if(args[1] != null) {
+                        Player target = Bukkit.getPlayer(args[1]);
+                        if(target == null) {
+                            sender.sendMessage("§cIs he online?");
+                            return true;
+                        }
+                        for(int x = 0; x < 360; x++) {
+                            target.sendMessage("");
+                        }
+                        sender.sendMessage("§aSuccessfully cleared player's chat!");
+                    } else {
+                        for(Player online : Bukkit.getOnlinePlayers()) {
+                            for(int x = 0; x < 360; x++) {
+                                online.sendMessage("");
+                            }
+                            sender.sendMessage("§aSuccessfully cleared global chat!");
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
